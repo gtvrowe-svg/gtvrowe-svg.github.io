066421363e025e17d00cae1e5cf7f9b8c4a4f714
gtvrowe-svg <gtv.rowe@gmail.com>
2026-09-21 20:41:32 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyAlFzxCuBlLMbvc8tA9oyE3cuab48kcY5Y",
   authDomain: "balanco-financeiro-sr-sra-rowe.firebaseapp.com",
