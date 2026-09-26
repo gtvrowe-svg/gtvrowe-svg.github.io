21ccb75685d97b55d72528f347c71c9e5f81c7a1
gtvrowe-svg <gtv.rowe@gmail.com>
2026-09-26 00:36:06 -0300

    Add files via upload

--- jardim/sw.js
+++ jardim/sw.js
@@ -1,5 +1,5 @@
-const C='jardim-v1';
-const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const C='jardim-v3';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-maskable.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))});
